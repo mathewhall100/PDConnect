f88fb165ab30adfcd799341e89484b4f9bbe8fef
ShiKwan <31040895+ShiKwan@users.noreply.github.com>
2018-10-29 14:49:54 -0400

    created a constant consist of medication and clinical trial

---
+++ client/src/constants.js
@@ -0,0 +1,72 @@
+export const medications = [
+    {
+        name : 'Botox',
+        description: 'drooling dyskinesia',
+        image : '',
+        side_effect : 
+            [
+                'side effect 1',
+                'side effect 2',
+            ],
+        clinical_trial_data : '',
+        drug_company : {
+            name : '',
+            phone : '',
+            website : '',
+            email : '',
+        }
+    },
+    {
+        name : 'Rytary',
+        description : 'Post Sinemet',
+        image : '',
+        side_effect :
+            [
+                'side effect 1',
+                'side effect 2',
+            ],
+        clinical_trial_data : '',
+        drug_company: {
+            name : '',
+            phone : '',
+            website : '',
+            email : '',
+        }   
+    }
+];
+
+
+export  const clinicial_trial = [
+    {
+        name : 'Spark',
+        description : '',
+        image : '',
+        side_effect : 
+            [
+
+            ],
+        clinical_trial_data : '',
+        drug_company : {
+            name : '',
+            phone : '',
+            website : '',
+            email : '',
+        }
+    },
+    {
+        name : 'Nylo',
+        description : '40-79 years old patient',
+        image : '',
+        side_effect :
+            [
+                
+            ],
+        clinical_trial_data : '',
+        drug_company : {
+            name : '',
+            phone : '',
+            website : '',
+            email : '',
+        }
+    }
+]
