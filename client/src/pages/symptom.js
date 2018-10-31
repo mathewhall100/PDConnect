10562c71a3149b840a2b36cc7e542db0df0091fe
Shi-Kwan <sktan@insytecg.com>
2018-10-31 12:59:09 -0400

    more work done on symptoms

--- client/src/pages/symptom.js
+++ client/src/pages/symptom.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { symptoms } from '../constants';
+import Button from '@material-ui/core/Button';
 import DoneIcon from '@material-ui/icons/Done';
 import Chip from '@material-ui/core/Chip';
 import PropTypes from 'prop-types';
@@ -8,6 +9,7 @@ import _ from 'lodash';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { withRouter, Redirect } from 'react-router-dom';
+import { submitSymptoms } from '../actions/SymptomsAction';
 
 const styles = theme => ({
     root: {
@@ -16,6 +18,9 @@ const styles = theme => ({
         flexWrap: 'wrap',
     },
     chip: {
+        margin: theme.spacing.unit,
+    },
+    button: {
         margin: theme.spacing.unit,
     },
 });
@@ -30,21 +35,64 @@ const styles = theme => ({
     }
     moveToUsedPool = (name) => {
         console.log("here");
+        let toSplice = Infinity;
         let used_symptoms_pool = this.state.used_symptoms_pool;
         let unused_symptoms_pool = this.state.unused_symptoms_pool;
         console.log("used and unused : ", used_symptoms_pool, unused_symptoms_pool);
-        _.forEach(unused_symptoms_pool, function(unused) {
-            if(unused.name === name){
-                used_symptoms_pool.push(name);
-                _.pull(unused_symptoms_pool, name)
-                console.log(unused_symptoms_pool);
-            }
-        })
+        if(unused_symptoms_pool.length > 0){
+            _.forEach(unused_symptoms_pool, function (unused, index) {
+                console.log("unused : ", unused);
+                if (unused.name === name) {
+                    used_symptoms_pool.push(unused);
+                    toSplice = index;
+                }
+            })
+            unused_symptoms_pool.splice(toSplice, 1);
+        }
+        
          console.log("used and unused : ", used_symptoms_pool, unused_symptoms_pool);
          this.setState({
              used_symptoms_pool : used_symptoms_pool,
              unused_symptoms_pool : unused_symptoms_pool,
          })
+    }
+     moveToUnusedPool = (name) => {
+         console.log("here");
+         let toSplice = Infinity;
+         let used_symptoms_pool = this.state.used_symptoms_pool;
+         let unused_symptoms_pool = this.state.unused_symptoms_pool;
+         console.log("used and unused : ", used_symptoms_pool, unused_symptoms_pool);
+         if(used_symptoms_pool.length > 0){
+             _.forEach(used_symptoms_pool, function (used, index) {
+                 if (used.name === name) {
+                     unused_symptoms_pool.push(used);
+                     toSplice = index;
+                     console.log("tosplice : ", toSplice);
+                 }
+             })
+
+             used_symptoms_pool.splice(toSplice, 1);
+             console.log("after splice : ", unused_symptoms_pool);
+         }
+         
+         console.log("used and unused : ", used_symptoms_pool, unused_symptoms_pool);
+         this.setState({
+             used_symptoms_pool: used_symptoms_pool,
+             unused_symptoms_pool: unused_symptoms_pool,
+         })
+     }
+     renderMessage = () => {
+         if(this.state.used_symptoms_pool.length > 0){
+             return(
+                 <Grid item xs={12}>
+                    You have selected : 
+                 </Grid>
+             )
+         }
+     }
+     submitSymptom = () => {
+        let used_symptoms_pool = this.state.used_symptoms_pool;
+        this.props.submitSymptom();
      }
     render() {
         const { classes } = this.props;
@@ -52,22 +100,26 @@ const styles = theme => ({
             return (<Chip key={i.name} label={i.name} clickable deleteIcon={<DoneIcon />} onClick={()=> this.moveToUsedPool(`${i.name}`)} className={classes.chip} />);
         })
         const used_symptoms_pool = this.state.used_symptoms_pool.map((i, index) => {
-            return (<Chip key={i} label={i} clickable onDelete={this.handleDelete} onClick={() => this.moveToUsedPool(`${i}`)} className={classes.chip} />);
+            return (<Chip key={i.name} label={i.name} clickable onDelete={this.handleDelete} onClick={() => this.moveToUnusedPool(`${i.name}`)} className={classes.chip} />);
         })
         return (
             <Grid container spacing={24}>
                 <h3>Pick the problems that you experience</h3>
                 <Grid container spacing={24}>
+                    {this.renderMessage()}
+                    <Grid item xs={12}>
+                        {used_symptoms_pool}
+                    </Grid>
+                    <Grid item xs={12}>
+                        These are some most common symptoms that Parkinsons patient suffer from.
+                    </Grid>
                     <Grid item xs={12}>
                         {unused_symptoms_pool}
                     </Grid>
-                </Grid>
-                <Grid container spacing={24}>
                     <Grid item xs={12}>
-                        These are some most common symptoms that Parkinsons patient suffer from. 
-                    </Grid>
-                    <Grid item xs={12}>
-                    {used_symptoms_pool}
+                        <Button type="submit" variant="outlined" onClick={()=> this.submitSymptom()} className={styles.Button}>
+                            See Results
+                        </Button>
                     </Grid>
                 </Grid>
             </Grid>
@@ -82,4 +134,4 @@ Symptom.propTypes = {
 };
 Symptom = withStyles(styles)(Symptom);
 Symptom = withRouter(Symptom);
-export default connect(mapStatsToProps, { })(Symptom);
+export default connect(mapStatsToProps, { submitSymptoms })(Symptom);
---
+++ client/src/reducers/SymptomsReducer.js
@@ -0,0 +1,19 @@
+import {
+    SUFFERED_SYMPTOMS
+} from '../actions/types';
+
+const INITIAL_STATE = {
+    sufferedSymptoms: [],
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case SUFFERED_SYMPTOMS:
+            return { ...INITIAL_STATE, sufferedSymptoms : action.payload }
+        default:
+            return state;
+    }
+}
+
+
+SUFFERED_SYMPTOMS
