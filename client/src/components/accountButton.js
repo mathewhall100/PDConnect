be8f94473e39dc110b3462ad0765e4fbb2948435
Shi-Kwan <38438280+1704043f@users.noreply.github.com>
2018-12-03 09:33:08 -0500

    added hover over effect on the my account button.

--- client/src/components/accountButton.js
+++ client/src/components/accountButton.js
@@ -12,16 +12,24 @@ import { home2Stylesheet, PRIMARY_COLOR } from '../styles.js';
 class FadeMenu extends React.Component {
     state = {
         anchorEl: null,
+        open : false,
         redirect : false,
         redirectUrl : '',
+        mouseOverButton: false,
+        mouseOverMenu: false,
     };
 
     handleClick = event => {
-        this.setState({ anchorEl: event.currentTarget });
+        this.setState({
+            open: true,
+            anchorEl: event.currentTarget
+        });
     };
 
     handleClose = () => {
-        this.setState({ anchorEl: null });
+        this.setState({ anchorEl: null,
+            mouseOverButton: false,
+            mouseOverMenu: false });
     };
     handleRedirect = (url) => {
         this.setState({
@@ -30,6 +38,25 @@ class FadeMenu extends React.Component {
             this.props.history.push(url);
         })
     }
+    enterButton = () => {
+        this.setState({ mouseOverButton: true });
+    }
+    leaveButton = () => {
+        // Set a timeout so that the menu doesn't close before the user has time to
+        // move their mouse over it
+        setTimeout(() => {
+            this.setState({ mouseOverButton: false });
+        }, 333);
+    }
+    enterMenu = () => {
+        this.setState({ mouseOverMenu: true });
+    }
+    leaveMenu = () => {
+        setTimeout(() => {
+            this.setState({ mouseOverMenu: false });
+        }, 333);
+    }
+
     handleLogout() {
         console.log("logging out...");
     }
@@ -37,16 +64,20 @@ class FadeMenu extends React.Component {
     render() {
         const { anchorEl, redirect, redirectUrl } = this.state;
         const { classes } = this.props;
-        const open = Boolean(anchorEl);
-
+        //const open = Boolean(anchorEl);
+        const open = this.state.mouseOverButton || this.state.mouseOverMenu;
 
         return (
-            <React.Fragment >
+            <React.Fragment>
                 <Button
                     className={classes.topBarBtn} style={{ float: "right", marginTop: "30px" }}
                     aria-owns={open ? 'fade-menu' : undefined}
                     aria-haspopup="true"
                     onClick={this.handleClick}
+                    onMouseEnter={this.enterButton}
+                    onMouseLeave={this.leaveButton}
+                    onMouseOver ={this.handleClick}
+
                 >
                     <AccountIcon style={{ fontSize: "24px", color: PRIMARY_COLOR }} /> &nbsp;&nbsp;My account
                 </Button>
@@ -55,12 +86,16 @@ class FadeMenu extends React.Component {
                     anchorEl={anchorEl}
                     open={open}
                     onClose={this.handleClose}
+                    MenuListProps={{
+                        onMouseEnter: this.enterMenu,
+                        onMouseLeave: this.leaveMenu,
+                    }}
                     TransitionComponent={Fade}
                 >
                     <MenuItem disable="true" style={{width:"110px", color: "black", fontSize: "14px", fontWeight: "bold"}} onClick={()=> this.handleRedirect('/profile')}><AccountIcon style={{ fontSize: "24px", color: PRIMARY_COLOR }} /> &nbsp;&nbsp;My account</MenuItem> <hr style={{margin: 0, padding: 0}}/>
                     <MenuItem style={{width:"110px", color: "black", fontSize: "14px", fontWeight: "bold"}} onClick={()=> this.handleRedirect('/profile')}>VIEW</MenuItem>
                     <MenuItem style={{width:"110px", color: "black", fontSize: "14px", fontWeight: "bold"}} onClick={()=> this.handleRedirect('/setting')}>SETTINGS</MenuItem>
-                   
+
                     <MenuItem style={{width:"110px", color: "black", fontSize: "14px", fontWeight: "bold"}} onClick={() => this.handleLogout()}>LOGOUT</MenuItem>
                 </Menu>
             </React.Fragment>
